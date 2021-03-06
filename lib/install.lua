d7ba00b564d3e3b39f0f57e7d00a13fdc27f91ff
Robin <midouesterner@gmail.com>
2021-03-06 12:35:25 -0600

    fix: update downloaded UGen version

--- lib/install.lua
+++ lib/install.lua
@@ -1,12 +1,12 @@
 local Installer = {
     PLUGIN = {
-        BASE_URL = "https://github.com/midouest/bytebeat/releases/download/",
-        VERSION = "0.0.2",
-        NAME = "ByteBeat",
-        ASSET_EXT = ".tar.gz"
+        BASE_URL = 'https://github.com/midouest/bytebeat/releases/download/',
+        VERSION = '0.1.0',
+        NAME = 'ByteBeat',
+        ASSET_EXT = '.tar.gz'
     },
-    STAGING_DIR = "/tmp/",
-    INSTALL_DIR = "/home/we/.local/share/SuperCollider/Extensions/"
+    STAGING_DIR = '/tmp/',
+    INSTALL_DIR = '/home/we/.local/share/SuperCollider/Extensions/'
 }
 
 local State = {
@@ -25,11 +25,11 @@ end
 
 local function get_url()
     local asset = get_asset_filename()
-    return Installer.PLUGIN.BASE_URL .. Installer.PLUGIN.VERSION .. "/" .. asset
+    return Installer.PLUGIN.BASE_URL .. Installer.PLUGIN.VERSION .. '/' .. asset
 end
 
 local function get_download_cmd(url)
-    return "wget -q -P " .. Installer.STAGING_DIR .. " " .. url
+    return 'wget -q -P ' .. Installer.STAGING_DIR .. ' ' .. url
 end
 
 local function download_plugin(callback)
@@ -41,12 +41,12 @@ end
 local function get_decompress_cmd()
     local staging = Installer.STAGING_DIR
     local asset = get_asset_filename()
-    return "tar -xf " .. staging .. asset .. " -C " .. staging
+    return 'tar -xf ' .. staging .. asset .. ' -C ' .. staging
 end
 
 local function get_install_cmd()
-    local staging_dir = Installer.STAGING_DIR .. "/" .. Installer.PLUGIN.NAME
-    return "cp -R " .. staging_dir .. " " .. Installer.INSTALL_DIR
+    local staging_dir = Installer.STAGING_DIR .. '/' .. Installer.PLUGIN.NAME
+    return 'cp -R ' .. staging_dir .. ' ' .. Installer.INSTALL_DIR
 end
 
 local function install_plugin()
@@ -66,12 +66,12 @@ end
 local function get_manifest()
     local ext_base = Installer.INSTALL_DIR
     local name = Installer.PLUGIN.NAME
-    local plugin_base = ext_base .. name .. "/" .. name .. "/"
-    local class_base = plugin_base .. "Classes/"
+    local plugin_base = ext_base .. name .. '/' .. name .. '/'
+    local class_base = plugin_base .. 'Classes/'
 
-    local plug = plugin_base .. "ByteBeat_scsynth.so"
-    local ugen = class_base .. "ByteBeat.sc"
-    local ctrl = class_base .. "ByteBeatController.sc"
+    local plug = plugin_base .. 'ByteBeat_scsynth.so'
+    local ugen = class_base .. 'ByteBeat.sc'
+    local ctrl = class_base .. 'ByteBeatController.sc'
 
     return {plug, ugen, ctrl}
 end
@@ -101,17 +101,19 @@ end
 
 function Installer.install(options)
     state = State.DOWNLOADING
-    download_plugin(function()
-        if options.on_downloaded then
-            options.on_downloaded()
+    download_plugin(
+        function()
+            if options.on_downloaded then
+                options.on_downloaded()
+            end
+            state = State.INSTALLING
+            install_plugin()
+            if options.on_installed then
+                options.on_installed()
+            end
+            state = State.SHUTTING_DOWN
         end
-        state = State.INSTALLING
-        install_plugin()
-        if options.on_installed then
-            options.on_installed()
-        end
-        state = State.SHUTTING_DOWN
-    end)
+    )
 end
 
 return Installer
